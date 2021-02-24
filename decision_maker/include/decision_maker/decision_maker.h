77a0de07e0a60d7b508db43007aa6950572a48f9
jiohLee <jiosiro@gmail.com>
2021-02-24 20:17:59 +0900

    add onLaneCallback & obstacleCallback

--- decision_maker/include/decision_maker/decision_maker.h
+++ decision_maker/include/decision_maker/decision_maker.h
@@ -5,6 +5,12 @@
 #include <sensor_msgs/LaserScan.h>
 #include <std_msgs/Float64.h>
 #include <std_msgs/String.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/conversions.h>
 
 #define _USE_MATH_DEFINES
 #include <math.h>
@@ -18,16 +24,37 @@ private:
     // ROS Callbacks
     void timerCallback(const ros::TimerEvent& event);
     void targetSteerCallback(const std_msgs::Float64::ConstPtr& msg);
+    void obstacleCallback(const sensor_msgs::PointCloud2::ConstPtr& msg);
+    void onLaneCallback(const std_msgs::String::ConstPtr& msg);
 
     // ROS Service
     ros::Publisher pubCmdVel;
     ros::Subscriber subTargetSteer;
+    ros::Subscriber subObstacle;
+    ros::Subscriber subOnLane;
     ros::Timer timer;
+
+    // LaneType
+    enum OnLaneType
+    {
+        ON_LANE_LEFT = 0,
+        ON_LANE_RIGHT
+    };
+    enum GoLaneType
+    {
+        GO_LANE_LEFT = 0,
+        GO_LANE_RIGHT
+    };
 
     // Variables
     double targetVel;
     double targetSteer;
     double velocity;
+
+    OnLaneType onLane;
+    OnLaneType currentLane;
+    GoLaneType goLane;
+    pcl::PointCloud<pcl::PointXYZI> centeroids;
 };
 
 #endif // DECISION_MAKER_H
--- decision_maker/src/decision_maker.cpp
+++ decision_maker/src/decision_maker.cpp
@@ -5,9 +5,13 @@ DecisionMaker::DecisionMaker(ros::NodeHandle & nh, ros::NodeHandle & pnh)
 {
     pubCmdVel = nh.advertise<geometry_msgs::Twist>("/decision_maker/cmd_vel", 1);
     subTargetSteer = nh.subscribe("/lane_detection/target_steer", 1, &DecisionMaker::targetSteerCallback, this);
+    subOnLane = nh.subscribe("/lane_detection/on_lane", 1, &DecisionMaker::onLaneCallback, this);
+    subObstacle = nh.subscribe("/obstacle_detection/point_cloud_cluster_centeroids", 1, &DecisionMaker::obstacleCallback, this);
 
     timer = nh.createTimer(ros::Duration(1.0 / 20.0), &DecisionMaker::timerCallback, this);
     pnh.param<double>("target_velocity", targetVel, 0.8);
+
+    goLane = GO_LANE_RIGHT;
 
     targetSteer = 0;
 }
@@ -15,6 +19,9 @@ DecisionMaker::DecisionMaker(ros::NodeHandle & nh, ros::NodeHandle & pnh)
 void DecisionMaker::timerCallback(const ros::TimerEvent &)
 {
     geometry_msgs::Twist target;
+
+
+
     velocity = targetVel;
     target.linear.x = velocity;
     target.angular.z = targetSteer;
@@ -25,4 +32,23 @@ void DecisionMaker::targetSteerCallback(const std_msgs::Float64::ConstPtr &msg)
 {
     const std_msgs::Float64& steer = *msg;
     targetSteer = steer.data;
+}
+
+void DecisionMaker::obstacleCallback(const sensor_msgs::PointCloud2::ConstPtr &msg)
+{
+    const sensor_msgs::PointCloud2& pcd = *msg;
+    pcl::fromROSMsg(pcd, centeroids);
+}
+
+void DecisionMaker::onLaneCallback(const std_msgs::String::ConstPtr &msg)
+{
+    const std_msgs::String& lane = *msg;
+    if(lane.data == "ON_LANE_LEFT")
+    {
+        onLane = ON_LANE_LEFT;
+    }
+    else if(lane.data == "ON_LANE_RIGHT")
+    {
+        onLane = ON_LANE_RIGHT;
+    }
 }
