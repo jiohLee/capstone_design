6eb5f9ad5bca1771246995e4c120ce446a74a659
jiohLee <jiosiro@gmail.com>
2021-04-27 00:08:05 +0900

    modify PID gains

--- decision_maker/include/decision_maker/decision_maker.h
+++ decision_maker/include/decision_maker/decision_maker.h
@@ -1,26 +1,21 @@
 #ifndef DECISION_MAKER_H
 #define DECISION_MAKER_H
 
-#include <vector>
-
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <std_msgs/Float64.h>
 #include <std_msgs/String.h>
-//#include <std_msgs/UInt8MultiArray.h>
-//#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/PointCloud2.h>
 
-//#include <pcl/point_cloud.h>
-//#include <pcl/point_types.h>
-//#include <pcl_conversions/pcl_conversions.h>
-//#include <pcl/conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/conversions.h>
 
 #define _USE_MATH_DEFINES
 #include <math.h>
 
 #include "decision_maker/controller.h"
-
-#include <obstacle_msgs/Obstacle.h>
 
 class DecisionMaker
 {
@@ -31,24 +26,17 @@ private:
     // ROS Callbacks
     void timerCallback(const ros::TimerEvent& event);
     void targetSteerCallback(const std_msgs::Float64::ConstPtr& msg);
-    void obstacleCallback(const obstacle_msgs::Obstacle::ConstPtr& msg);
+    void obstacleCallback(const sensor_msgs::PointCloud2::ConstPtr& msg);
     void onLaneCallback(const std_msgs::String::ConstPtr& msg);
 
     // ROS Service
+//    ros::Publisher pubCmdVel;
     ros::Subscriber subTargetSteer;
     ros::Subscriber subObstacle;
     ros::Subscriber subOnLane;
     ros::Timer timer;
 
-    // ROS Messages
-    obstacle_msgs::Obstacle obstacles;
-
-    // ROS Param
-    double targetVel;
-    double curveThresholdSteer;
-    double curveThresholdTime;
-
-    // Enums
+    // LaneType
     enum OnLaneType
     {
         ON_LANE_LEFT = 1,
@@ -59,20 +47,11 @@ private:
         GO_LANE_LEFT = 1,
         GO_LANE_RIGHT = -1
     };
-    enum LaneType
-    {
-        LANE_CURVE = -1,
-        LANE_STRAIGHT = 1
-    };
-
-    // Functions
-    void switchLane();
-    bool isCarDetected(geometry_msgs::Point& pt);
-    bool isObstacleAhead(double lowerRange, double upperRange, OnLaneType olt);
 
     // Variables
     Controller ctrl;
 
+    double targetVel;
     double targetSteer;
     double velocity;
     double steer;
@@ -80,12 +59,7 @@ private:
     OnLaneType onLane;
     OnLaneType currentLane;
     GoLaneType goLane;
-    LaneType lane;
-    ros::Time timePointLaneCheck;
-    ros::Time timePointStraightCheck;
-
-    bool car;
-    geometry_msgs::Point carPoint;
+    pcl::PointCloud<pcl::PointXYZI> centeroids;
 };
 
 #endif // DECISION_MAKER_H
--- decision_maker/src/decision_maker.cpp
+++ decision_maker/src/decision_maker.cpp
@@ -6,110 +6,43 @@ DecisionMaker::DecisionMaker(ros::NodeHandle & nh, ros::NodeHandle & pnh)
 {
     subTargetSteer = nh.subscribe("/lane_detection/target_steer", 1, &DecisionMaker::targetSteerCallback, this);
     subOnLane = nh.subscribe("/lane_detection/on_lane", 1, &DecisionMaker::onLaneCallback, this);
-    subObstacle = nh.subscribe("/obstacle_detection/obstacle", 1, &DecisionMaker::obstacleCallback, this);
+    subObstacle = nh.subscribe("/obstacle_detection/point_cloud_cluster_centeroids", 1, &DecisionMaker::obstacleCallback, this);
 
     timer = nh.createTimer(ros::Duration(1.0 / 20.0), &DecisionMaker::timerCallback, this);
     pnh.param<double>("target_velocity", targetVel, 0.8);
-    pnh.param<double>("curve_threshold_steer", curveThresholdSteer, 0.15);
-    pnh.param<double>("curve_threshold_time", curveThresholdTime, 0.5);
 
-    goLane = GO_LANE_RIGHT; // start on the right lane
-
-    car = false;
+    goLane = GO_LANE_RIGHT;
 
     targetSteer = 0;
-    timePointLaneCheck = ros::Time::now();
 }
 
 void DecisionMaker::timerCallback(const ros::TimerEvent &)
 {
-    // init values
+    geometry_msgs::Twist target;
     velocity = targetVel;
     steer = targetSteer;
 
-    // check lane type
-    std::string laneTypeMsg = "UNKNOWN";
-    double laneDur = ros::Time::now().toSec() - timePointLaneCheck.toSec();
-    if(std::abs(targetSteer) > curveThresholdSteer)
+    // stop when obstacle exist
+    bool obstacle = false;
+    for(size_t i = 0; i < centeroids.size(); i++)
     {
-        timePointLaneCheck = ros::Time::now();
-        lane = LANE_CURVE;
-        laneTypeMsg = "CURVE";
-    }
-    if(laneDur > curveThresholdTime)
-    {
-        lane = LANE_STRAIGHT;
-        laneTypeMsg = "STRAIGHT";
-    }
-    ROS_INFO("LANE : %s", laneTypeMsg.c_str());
-
-    if(isObstacleAhead(0, 1.0, onLane))
-    {
-        if(car)
+        const pcl::PointXYZI& pt = centeroids[i];
+        if(
+                (0.0f < pt.x && pt.x < 0.5f) &&
+                (-0.25f < pt.y && pt.y < 0.25f)
+                )
         {
-            ROS_INFO("CAR AHEAD");
-            if(carPoint.x < 0.7)
-            {
-                switchLane();
-            }
+            obstacle = true;
         }
-        else
-        {
-            ROS_INFO("OBSACLE AHEAD");
-        }
+    }
+    if(obstacle)
+    {
+        velocity = 0;
+        ROS_WARN("OBSTACLE AHEAD");
     }
     else
     {
         ROS_INFO("OBSTACLE FREE");
-        if(onLane == ON_LANE_LEFT)
-        {
-            switchLane();
-        }
-    }
-
-    // info
-    if(onLane == ON_LANE_LEFT)
-    {
-        ROS_INFO("ON_LANE_LEFT");
-        velocity = 0.8;
-    }
-    else
-    {
-        velocity = 0.6;
-        ROS_INFO("ON_LANE_RIGHT");
-    }
-    if(goLane == GO_LANE_LEFT)
-    {
-        ROS_INFO("GO_LANE_LEFT");
-    }
-    else
-    {
-        ROS_INFO("GO_LANE_RIGHT");
-    }
-
-    // follow order
-    if(goLane == GO_LANE_RIGHT)
-    {
-        if(onLane == ON_LANE_LEFT)
-        {
-            steer = GO_LANE_RIGHT * 0.3;
-            velocity = 0.4;
-        }
-    }
-    else if(goLane == GO_LANE_LEFT)
-    {
-        if(onLane == ON_LANE_RIGHT)
-        {
-            steer = GO_LANE_LEFT * 0.3;
-            velocity = 0.4;
-        }
-    }
-
-    // estop
-
-    if(isObstacleAhead(0, 0.5, onLane))
-    {
-        velocity = 0;
     }
 
     if(!ctrl.isMenual())
@@ -126,19 +59,10 @@ void DecisionMaker::targetSteerCallback(const std_msgs::Float64::ConstPtr &msg)
     targetSteer = steer.data;
 }
 
-void DecisionMaker::obstacleCallback(const obstacle_msgs::Obstacle::ConstPtr &msg)
+void DecisionMaker::obstacleCallback(const sensor_msgs::PointCloud2::ConstPtr &msg)
 {
-    obstacles = *msg;
-    geometry_msgs::Point pt;
-    if(isCarDetected(pt))
-    {
-        car = true;
-        carPoint = pt;
-    }
-    else
-    {
-        car = false;
-    }
+    const sensor_msgs::PointCloud2& pcd = *msg;
+    pcl::fromROSMsg(pcd, centeroids);
 }
 
 void DecisionMaker::onLaneCallback(const std_msgs::String::ConstPtr &msg)
@@ -152,77 +76,4 @@ void DecisionMaker::onLaneCallback(const std_msgs::String::ConstPtr &msg)
     {
         onLane = ON_LANE_RIGHT;
     }
-}
-
-void DecisionMaker::switchLane()
-{
-    if(lane == LANE_STRAIGHT)
-    {
-        ROS_INFO("READY TO SWITCH");
-        if(!isObstacleAhead(-1.0, 1.0, static_cast<OnLaneType>(onLane * -1)))
-        {
-            ROS_INFO("SWITCHING");
-            if(onLane == ON_LANE_LEFT)
-            {
-                goLane = GO_LANE_RIGHT;
-            }
-            else if(onLane == ON_LANE_RIGHT)
-            {
-                goLane = GO_LANE_LEFT;
-            }
-        }
-        else
-        {
-            ROS_INFO("CANNOT SWITCH");
-        }
-    }
-}
-
-bool DecisionMaker::isCarDetected(geometry_msgs::Point &pt)
-{
-    for(size_t i = 0; i < obstacles.labels.size(); i++)
-    {
-        if(obstacles.labels[i].data == 1)
-        {
-            pt = obstacles.centeroids[i];
-            return true;
-        }
-    }
-    return false;
-}
-
-bool DecisionMaker::isObstacleAhead(double lowerRange, double upperRange, DecisionMaker::OnLaneType olt)
-{
-    double yRangeLeft = -0.25;
-    double yRangeRight = 0.25;
-
-    if(olt == ON_LANE_LEFT)
-    {
-        if(onLane == ON_LANE_RIGHT)
-        {
-            yRangeLeft += 0.4;
-            yRangeRight += 0.4;
-        }
-    }
-    else if(olt == ON_LANE_RIGHT)
-    {
-        if(onLane == ON_LANE_LEFT)
-        {
-            yRangeLeft -= 0.4;
-            yRangeRight -= 0.4;
-        }
-    }
-
-    for(size_t i = 0; i < obstacles.centeroids.size(); i++)
-    {
-        const geometry_msgs::Point& pt = obstacles.centeroids[i];
-        if(
-                (lowerRange < pt.x && pt.x < upperRange) &&
-                (yRangeLeft < pt.y && pt.y < yRangeRight)
-                )
-        {
-            return true;
-        }
-    }
-    return false;
 }
